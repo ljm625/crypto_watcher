10a6581329798cabdf5e004b128c93533297b40f
Jiaming Li <ljm625@gmail.com>
2020-04-18 18:30:17 +0800

    first Working Version

---
+++ Exchanges/Bitmex.py
@@ -0,0 +1,179 @@
+import base64
+import hashlib
+import hmac
+import json
+import time
+import urllib.parse
+
+import aiohttp
+import websockets
+
+
+class Bitmex(object):
+
+    def __init__(self,account_name,api_token,pair,api_secret=None,testnet=False):
+        self.account=account_name
+        self.pair=pair
+        self.api_token=api_token
+        self.api_secret = api_secret
+        if testnet:
+            self.base_url="https://testnet.bitmex.com"
+            self.websocket_url = "wss://testnet.bitmex.com/realtime"
+        else:
+            self.base_url="https://www.bitmex.com"
+            self.websocket_url = "wss://www.bitmex.com/realtime"
+
+        if not self.api_secret:
+            raise Exception("Please provide API Secret")
+        pass
+
+        self.exchange = None
+
+
+    def generate_signature(self,payload_raw):
+        return hmac.new(bytes(self.api_secret, 'utf8'), bytes(payload_raw, 'utf8'), digestmod=hashlib.sha256).hexdigest()
+
+
+    async def _api_wrapper(self,method,endpoint,payload):
+        url = "{}{}".format(self.base_url,endpoint)
+        expire = str(int(time.time())+10)
+        verb = method.upper()
+
+        payload_raw = verb+endpoint+str(expire)+json.dumps(payload)
+
+        signature=self.generate_signature(payload_raw)
+        # headers
+        headers = {
+            'api-expires': expire,
+            'api-key': self.api_token,
+            'api-signature': signature
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.request(method,url, headers=headers, json=payload) as resp:
+                if resp.status==200:
+                    result = await resp.json()
+                    return result
+                else:
+                    try:
+                        result = await resp.json()
+                    except Exception as e:
+                        raise Exception("Exception when POST {} {} {}".format(endpoint, resp.status,e))
+                    raise Exception("Exception when POST {}  {}".format(endpoint, result["error"]["message"]))
+
+    async def _get_wrapper(self,endpoint,data={}):
+        expire = str(int(time.time())+10)
+        verb = "GET"
+        if urllib.parse.urlencode(data)!="":
+            endpoint+="?"+urllib.parse.urlencode(data)
+        url = "{}{}".format(self.base_url,endpoint)
+
+        payload_raw = verb+endpoint+str(expire)
+
+        signature=self.generate_signature(payload_raw)
+
+
+        # headers
+        headers = {
+            'api-expires': expire,
+            'api-key': self.api_token,
+            'api-signature': signature
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=headers) as resp:
+                if resp.status==200:
+                    result = await resp.json()
+                    return result
+                else:
+                    try:
+                        result = await resp.json()
+                    except Exception as e:
+                        raise Exception("Exception when POST {} {} {}".format(endpoint, resp.status,e))
+                    raise Exception("Exception when POST {}  {}".format(endpoint,result["error"]["message"]))
+
+
+    async def update_leverage(self,leverage):
+            payload={
+                "symbol":"XBTUSD",
+                "leverage":leverage
+            }
+            await self._api_wrapper("POST","/api/v1/position/leverage",payload)
+            return True
+
+    async def do_long(self,amount,price):
+        payload = {
+            "symbol":self.pair,
+            "orderQty":amount,
+            "price":price,
+            "ordType":"Limit"
+        }
+        result = await self._api_wrapper("POST","/api/v1/order",payload)
+        return result["orderID"]
+
+
+    async def do_short(self,amount,price):
+        payload = {
+            "symbol":self.pair,
+            "orderQty":-amount,
+            "price":price,
+            "ordType":"Limit"
+        }
+        result = await self._api_wrapper("POST","/api/v1/order",payload)
+        return result["orderID"]
+
+    async def cancel_order(self,order_id):
+        payload = {
+            "orderID":order_id,
+        }
+        await self._api_wrapper("DELETE","/api/v1/order",payload)
+        return True
+
+    async def cancel_all_orders(self):
+        payload = {
+            "symbol":self.pair,
+        }
+        await self._api_wrapper("DELETE","/api/v1/order/all",payload)
+        return True
+
+    async def get_balance(self,in_usd=True):
+        payload = {
+            "currency":"XBt"
+        }
+        result = await self._get_wrapper("/api/v1/user/margin",payload)
+        balance = result["availableMargin"]*0.00000001
+        if in_usd:
+            price = await self.get_ticker()
+            balance = price*balance
+        return balance
+
+    async def get_ticker(self):
+        payload = {
+            "symbol":"XBTUSD"
+        }
+        data = await self._get_wrapper("/api/v1/orderBook/L2",payload)
+        for order in data:
+            if order["side"]=="Buy":
+                return order["price"]
+        return 0
+
+    async def websocket(self,subcribes,handler,auth=True):
+        while True:
+            try:
+                uri = self.websocket_url
+                async with websockets.connect(uri) as websocket:
+                    if auth:
+                        expire = int(time.time()) + 10
+                        payload = 'GET/realtime' + str(expire)
+                        signature = self.generate_signature(payload)
+                        msg = {"op": "authKeyExpires", "args": [self.api_token, expire, signature]}
+                        await websocket.send(json.dumps(msg))
+
+                    ping=json.dumps({"op": "subscribe", "args": subcribes})
+                    await websocket.send(ping)
+                    while True:
+                        data = await websocket.recv()
+                        json_data=json.loads(data)
+                        handler(json_data)
+            except Exception as e:
+                print("ERROR: websocket faced issue: {}, auto respawn".format(e))
+                await self.websocket(subcribes,handler,auth)
+
--- bitmex_trader.py
+++ bitmex_trader.py
@@ -1,4 +1,6 @@
 import json
+
+import aiohttp
 import websockets
 import asyncio
 
@@ -7,16 +9,23 @@ from argparse import Namespace
 import yaml
 
 from BotNotifier import BotNotifier
+from Exchanges.Bitmex import Bitmex
 
 args = Namespace(
     bitmex_price=0,
     bxbt_price=0,
-    balance=2000,
-    position= [],
-    capital= 1000,
-    amount = 0.1,
-    leverage = 100,
-    sent = False
+    highest_gap={},
+    sent = False,
+    have_pos = False,
+    have_order = False,
+    api = None,
+    cur_leverage = 0,
+    order_id = None,
+    direction = "watch",
+    order_size = 0,
+    start_balance = 0,
+    bot=None,
+    blocker= False
 )
 config ={}
 bot = None
@@ -26,75 +35,148 @@ async def opportunity_finder():
         return "Bitmex: {} BXBT: {} GAP: {}%".format(args.bitmex_price,args.bxbt_price,gap_percentage*100)
     gap = abs(args.bitmex_price - args.bxbt_price)
     gap_percentage = gap / args.bitmex_price
-    msg = None
-    if gap_percentage>=0.05:
-        msg = "#100X Leverage NOW\n"
-        msg+=build_msg()
-    elif gap_percentage>=0.02:
-        msg = "#50X Leverage NOW\n"
-        msg+=build_msg()
-    elif gap_percentage>=0.01:
-        msg = build_msg()
-    if msg and args.sent:
-        await bot.notify(msg)
-        args.sent=True
+    if not args.highest_gap or gap_percentage>= args.highest_gap["percentage"]:
+        args.highest_gap = {
+            "percentage":gap_percentage,
+            "bitmex":args.bitmex_price,
+            "bxbt":args.bxbt_price
+        }
+    if args.bitmex_price > args.bxbt_price:
+        direction="sell"
+    else:
+        direction="buy"
+    if gap_percentage>=0.01:
+        await do_trade(direction,args.order_size,100)
+    elif gap_percentage>=0.005:
+        await do_trade(direction,args.order_size,50)
 
+async def order_ttl(order_id):
+    await asyncio.sleep(config["order_ttl"])
+    await args.api.cancel_order(order_id)
 
+async def unblock():
+    await asyncio.sleep(30)
+    args.blocker = False
 
-async def bitmex_ticker():
-    while True:
-        try:
-            uri = "wss://www.bitmex.com/realtime"
-            async with websockets.connect(uri) as websocket:
-                # name = input("What's your name? ")
-                ping=json.dumps({"op": "subscribe", "args": ["instrument:XBTUSD","instrument:.BXBT"]})
-                await websocket.send(ping)
-                while True:
-                    data = await websocket.recv()
-                    json_data=json.loads(data)
-                    # print(json.loads(data))
-                    if json_data.get('table')=='instrument':
-                        if json_data.get('data')[0]['symbol']=='XBTUSD':
-                            trade_data = json_data.get('data')[0]
-                            if trade_data.get("lastPrice"):
-                                args.bitmex_price = trade_data.get("lastPrice")
-                        elif json_data.get('data')[0]['symbol']=='.BXBT':
-                            trade_data = json_data.get('data')[0]
-                            if trade_data.get("lastPrice"):
-                                args.bxbt_price = trade_data.get("lastPrice")
-                            print("Bitmex/BXBT Price: {} {}".format(args.bitmex_price,args.bxbt_price))
-                            gap = abs(args.bitmex_price-args.bxbt_price)
-                            gap_percentage = gap/args.bitmex_price
-                            if gap >= 10:
-                                print("GAP: {} {}%".format(gap,gap_percentage*100))
-                            if gap_percentage >=0.01:
-                                print("OPPORTUNITY!!!")
-                            await opportunity_finder()
+async def do_trade(direction,amount,leverage):
+    # Blocker
+    if args.blocker:
+        return
+    else:
+        args.blocker = True
+        # Make sure if exception happens, it can still unblock
+        asyncio.ensure_future(unblock())
+    # Check before placing order
+    # Whether update order
+    if args.have_pos and args.direction == direction:
+        return
+    if args.have_order:
+        await args.api.cancel_order(args.order_id)
+    if args.cur_leverage != leverage:
+        await args.api.update_leverage(leverage)
+        args.cur_leverage = leverage
+    if direction=='buy':
+        order_id = await args.api.do_long(amount,args.bitmex_price)
+    else:
+        order_id = await args.api.do_short(amount,args.bitmex_price)
+    # Make sure order will be ignored after ttl.
+    args.have_order = True
+    args.order_id = order_id
+    asyncio.ensure_future(order_ttl(order_id))
+    msg = "#Order\nSubmitted {} Order at {} {}".format(direction.upper(),args.bitmex_price,amount)
+    await args.bot.notify(msg)
 
-        except:
-            pass
-
-            # if type(json_data)==list and json_data[1]!='hb':
-            #     print("Bitfinex Price: {}".format(json_data[1][6]))
-            #     args.bfx_price = float(json_data[1][6])
+    # args.blocker = False
 
 def load_config():
     with open("config.yaml") as file:
         cfg = yaml.safe_load(file.read())
         return cfg
 
-async def rate_limiter():
+def find_gap(data):
+    if data.get('data')[0]['symbol'] == 'XBTUSD':
+        trade_data = data.get('data')[0]
+        if trade_data.get("lastPrice"):
+            args.bitmex_price = trade_data.get("lastPrice")
+    elif data.get('data')[0]['symbol'] == '.BXBT':
+        trade_data = data.get('data')[0]
+        if trade_data.get("lastPrice"):
+            args.bxbt_price = trade_data.get("lastPrice")
+        print("Bitmex/BXBT Price: {} {}".format(args.bitmex_price, args.bxbt_price))
+        gap = abs(args.bitmex_price - args.bxbt_price)
+        gap_percentage = gap / args.bitmex_price
+        if gap_percentage >=0.001:
+            asyncio.ensure_future(opportunity_finder())
+
+def update_position(data):
+    if data.get('action') and data.get('action')=='update':
+        for pair in data['data']:
+            if pair['symbol']=="XBTUSD":
+                if pair['currentQty']!=0:
+                    args.have_pos = True
+                    if pair['currentQty']>0:
+                        args.direction="buy"
+                    else:
+                        args.direction = "sell"
+                elif pair['currentQty']==0:
+                    args.have_pos = False
+                if pair.get("posState") and pair["posState"]=="Liquidated":
+                    # Oh fuck its liquidated!
+                    if args.have_order and not args.have_pos:
+                        # Oh Fuck instant liquidate!
+                        args.have_order = False
+                    args.have_pos = False
+                    # Trigger Liquidated warning
+                    msg = "#Liquidation\nYour Position has been Liquidated at {}".format(args.bitmex_price)
+                    asyncio.ensure_future(args.bot.notify(msg))
+
+
+def update_order(data):
+    if data.get('action') and data.get('action')=='update':
+        for order in data["data"]:
+            if order["orderID"]==args.order_id:
+                if order.get("ordStatus") and order["ordStatus"]=="Filled":
+                    args.have_order = False
+                    msg = "#Order\nYour Order has been Filled at {}".format(args.bitmex_price)
+                    asyncio.ensure_future(args.bot.notify(msg))
+
+
+def handler_ws(data):
+    if data.get('table') == 'instrument':
+        find_gap(data)
+    elif data.get('table') == 'position':
+        update_position(data)
+    elif data.get('table') == 'order':
+        update_order(data)
+
+async def balance_checker():
     while True:
-        if args.sent:
-            args.sent = False
-        await asyncio.sleep(config["delta"])
+        await asyncio.sleep(3600)
+        balance = await args.api.get_balance()
+        if balance<= args.start_balance*0.2:
+            # Generate Warning Message.
+            msg = "#Warning\nYour BitMEX current balance is : {} which may cause issue on Bot. Please Check NOW.".format(balance)
+            await args.bot.notify(msg)
+
+
+async def main(cfg):
+    bm = Bitmex("test",cfg["bitmex_test_id"],"XBTUSD",cfg["bitmex_test_secret"],testnet=True)
+    args.api = bm
+    args.bot = BotNotifier(cfg["telegram_bot_api"],cfg["telegram_channel_id_trade"])
+    # Calculate order_size
+    args.start_balance = await bm.get_balance()
+    args.order_size = int(args.start_balance/cfg["money_split"])
+    # Check balance
+    asyncio.ensure_future(balance_checker())
+
+    await bm.websocket(["instrument:XBTUSD","instrument:.BXBT","position:XBTUSD","order:XBTUSD"],handler_ws)
+
+
 
 if __name__ == '__main__':
 
     config = load_config()
-    bot = BotNotifier(config["telegram_bot_api"],config["telegram_channel_id"])
-    coro = bitmex_ticker()
-    asyncio.ensure_future(coro)
-    asyncio.get_event_loop().run_forever()
+    coro = main(config)
+    asyncio.get_event_loop().run_until_complete(coro)
 
 
